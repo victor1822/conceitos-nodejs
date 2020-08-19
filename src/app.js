e14f5ed70567e34f74a58f66c4d707b22e91cda1
Victor <victsales@gmail.com>
2020-08-19 07:59:18 -0300

    feat:Create projects CRUD and likes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,73 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  // DONE
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  // DONE
+  const {title, url, techs} = request.body;
+
+  const repository = {
+    title,
+    url,
+    techs,
+    likes: 0,
+    id: uuid()
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  // DONE
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(repository => repository.id===id);
+
+  if(repositoryIndex < 0) return response.status(400).send()
+
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    title,
+    url,
+    techs,
+    id: repositories[repositoryIndex].id,
+    likes: repositories[repositoryIndex].likes
+  }
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  // DONE
+  const { id } = request.params;
+
+  repositoryIndex = repositories.findIndex(repo => repo.id === id);
+
+  if(repositoryIndex < 0) return response.status(400).send();
+
+  repositories.splice(repositoryIndex,1);
+
+  return response.status(204).send();
+  
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  // DONE
+  const { id } = request.params;
+
+  const repository = repositories.find(thisrepository => thisrepository.id===id);
+
+  if(!repository) return response.status(400).send();
+
+  repository.likes++;
+
+  return response.json(repository);
 });
 
 module.exports = app;
